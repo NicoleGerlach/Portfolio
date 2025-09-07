9dd35496e830fac3f6a076450b520b85f43bfec2
Nicole Gerlach <coleni82@gmail.com>
2025-09-07 13:37:04 +0200

    edit routes, projetcs component, scss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,9 @@
 
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
-import { WhyMeComponent } from './main-content/why-me/why-me.component';
-import { MySkillsComponent } from './main-content/my-skills/my-skills.component';
-import { MyProjectsComponent } from './main-content/my-projects/my-projects.component';
-import { ContactMeComponent } from './main-content/contact-me/contact-me.component';
 
 export const routes: Routes = [
     { path: '', component: MainContentComponent },
-    { path: 'whyMe', component: WhyMeComponent },
-    { path: 'skills', component: MySkillsComponent },
-    { path: 'projects', component: MyProjectsComponent },
-    { path: 'contact', component: ContactMeComponent },
 ];
 
 
