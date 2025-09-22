24322165eb7c9c7785dc94e5878958ee20617a7d
Nicole Gerlach <coleni82@gmail.com>
2025-09-22 20:16:36 +0200

    edit my-projects component

--- src/app/main-content/my-projects/my-projects.component.ts
+++ src/app/main-content/my-projects/my-projects.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { JoinComponent } from './join/join.component';
 
 @Component({
   selector: 'app-my-projects',
   standalone: true,
-  imports: [],
+  imports: [JoinComponent],
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.scss'
 })
