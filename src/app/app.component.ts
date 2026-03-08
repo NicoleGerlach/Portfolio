6f76b8c32bcb685901576d9ea370d60af4a15e67
Nicole Gerlach <coleni82@gmail.com>
2026-03-08 10:58:24 +0100

    save and load current language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './shared/footer/footer.component';
@@ -6,7 +6,9 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
-import { ImprintComponent } from './imprint/imprint.component'; 
+import { ImprintComponent } from './imprint/imprint.component';
+
+import { LanguageService } from './services/language.service';
 
 @Component({
   selector: 'app-root',
@@ -23,6 +25,21 @@ import { ImprintComponent } from './imprint/imprint.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Portfolio';
+export class AppComponent implements OnInit {
+  title = 'Nicole Gerlach';
+
+  constructor(private languageService: LanguageService) {}
+
+    ngOnInit() {
+    const savedLang = localStorage.getItem('lang') as 'de' | 'en' | null;
+    const initialLang: 'de' | 'en' = savedLang ?? 'de';
+    if (this.languageService.currentLang !== initialLang) {
+      this.setLang(initialLang);
+    }
+  }
+
+  private setLang(lang: 'de' | 'en') {
+    this.languageService.currentLang = lang;
+    localStorage.setItem('lang', lang);
+  }
 }
